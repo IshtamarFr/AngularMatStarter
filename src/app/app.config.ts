8703a9b7ba260f58b21f220953a92d144fbb20b2
IshtamarFr <ishta@ishtamar.fr>
2024-03-18 21:29:21 +0100

    added hash location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/common/http';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { LOCALE_ID } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,5 +19,6 @@ export const appConfig: ApplicationConfig = {
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     provideHttpClient(withInterceptorsFromDi()),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 };
