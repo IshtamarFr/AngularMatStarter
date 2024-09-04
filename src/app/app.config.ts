30d8d254da496701d78e5ea921cc7d6d83115631
IshtamarFr <ishta@ishtamar.fr>
2024-09-04 16:10:02 +0200

    cli upgrade 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { LOCALE_ID } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
